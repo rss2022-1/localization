1d8b49cabc0a37248f1fe745ec743f1eaf9de561
Alex <acelli@mit.edu>
2022-03-29 20:01:47 +0000

    added header

--- src/localization/particle_filter.py
+++ src/localization/particle_filter.py
@@ -157,6 +157,8 @@ class ParticleFilter:
         """ Publish a transform between the map and the base_link frome of the given pose. """
         # Publish this "average" pose as a transform between the map and the car's expected base_link
         new_pose = PoseWithCovarianceStamped()
+        new_pose.header.frame_id = "map"
+        new_pose.header.stamp = rospy.Time.now()
         new_pose.pose.pose.position.x = avg_pose[0]
         new_pose.pose.pose.position.y = avg_pose[1]
         new_pose.pose.pose.position.z = 0
